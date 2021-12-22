3dd6437cd5d5902fbea1a01a312999109815851b
Manish Jalui <68913884+manishjalui11@users.noreply.github.com>
2021-12-22 16:39:08 +0530

    Update settings.py

--- django_projects/settings.py
+++ django_projects/settings.py
@@ -136,5 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "manishexp11@gmail.com"
-EMAIL_HOST_PASSWORD = "umuxxoausogwdrve"
+EMAIL_HOST_USER = ""#enter email 
+EMAIL_HOST_PASSWORD = "" #enter password
